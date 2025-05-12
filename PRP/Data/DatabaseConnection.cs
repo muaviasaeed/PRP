c54683dd38bcd6aeee62d4ad537448c12c3f0eed
adil954 <40742414@live.napier.ac.uk>
2025-05-12 10:35:59 -0700

    Initial commit

---
+++ PRP/Data/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace PRP.Data  
+    public class DatabaseConnection
+    {
+        private readonly IConfiguration _config;
+
+        public DatabaseConnection()
+        {
+            _config = new ConfigurationBuilder()
+                .AddUserSecrets<DatabaseConnection>()
+                .Build();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(
+                $"server=localhost;" +
+                $"database=world;" +  
+                $"uid=root;" +
+                $"pwd={_config["DBPassword"]};" +
+                "Port=3306;" +
+                "SslMode=Required;");
+        }
+    }
+}
